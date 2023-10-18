548571ab2073bb70f5dcf713a08acccd22035b33
lernEarnBurn <lerners124@gmail.com>
2023-10-18 15:49:26 +0300

    connected db driver

--- blog-api/app.js
+++ blog-api/app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URI;
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 const app = express()
+
 
 app.get('/poop/pea', (req, res) => {
   res.json('hello world')
