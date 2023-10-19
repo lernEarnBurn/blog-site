af9ba4bec7e93688f02745c55f9ba634975711d2
lernEarnBurn <lerners124@gmail.com>
2023-10-19 15:49:14 +0300

    sign up route and logic

---
+++ blog-api/controllers/userController.js
@@ -0,0 +1,21 @@
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/user')
+
+//implement form sanitation and validation once front end is present
+exports.signUp = asyncHandler(async(req, res, next) => {
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) {
+      return next(err);
+    } else {
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+
+      const result = await user.save();
+      res.json(user);
+    }
+  });
+})
