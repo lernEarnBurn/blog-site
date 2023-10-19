bc6b1ccfd60f28d526509b581c33fac28d74277b
lernEarnBurn <lerners124@gmail.com>
2023-10-19 15:53:40 +0300

    prepped passport routes

--- blog-api/routers/authRouter.js
+++ blog-api/routers/authRouter.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
+
+router.post('/log-in',  passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/',
+}), (req, res) => { res.json('logged in') })
+
+router.get('/log-out', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.json('logged out');
+  });
+});
 
 
 module.exports = router
