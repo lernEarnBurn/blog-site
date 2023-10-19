1d650d4aabefa54b305b60c5c67e6d821f07a7a0
lernEarnBurn <lerners124@gmail.com>
2023-10-19 14:35:50 +0300

    data models

---
+++ blog-api/models/post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  title: {type: String, required: true},
+  content: {type: String, required: true}
+})
+
+postSchema.virtual('url').get(() => {
+  return `/posts/${this.id}`
+})
+
+module.exports =  mongoose.model('Post', postSchema)
