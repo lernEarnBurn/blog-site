1eef31028470a323e366c9b5577edd66a99648cb
lernEarnBurn <lerners124@gmail.com>
2023-10-19 14:46:53 +0300

    set up routers (crud operation)

--- blog-api/app.js
+++ blog-api/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 
+const createRouter = require('./routers/createRouter')
+const readRouter = require('./routers/readRouter')
+const updateRouter = require('./routers/updateRouter')
+const deleteRouter = require('./routers/deleteRouter')
+
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const { create } = require('./models/comment')
 mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGODB_URI;
 main().catch((err) => console.log(err));
@@ -12,10 +18,11 @@ async function main() {
 
 const app = express()
 
+app.use('/', createRouter)
+app.use('/', readRouter)
+app.use('/', updateRouter)
+app.use('/', deleteRouter)
 
-app.get('/poop/pea', (req, res) => {
-  res.json('hello world')
-})
 
 app.listen(3000, () => {
   'app listening on port 3000'
