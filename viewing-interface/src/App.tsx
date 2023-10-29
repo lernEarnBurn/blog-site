1670e4a14dcd8106831c43f668ac4b5be41ce6d0
lernEarnBurn <lerners124@gmail.com>
2023-10-29 15:23:49 +0200

    set up routing with react router dom

--- viewing-interface/src/App.tsx
+++ viewing-interface/src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useState } from 'react'
+
+import { NavBar } from './components/NavBar'
+import { LoginForm } from './components/LoginForm'
+import { SignupForm } from './components/SignupForm'
+import { BlogMenu } from './components/BlogMenu'
+import { BlogPage } from './components/BlogPage'
+
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
 
   return (
     <>
-     
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={ loggedIn ? ( <Navigate to="/blogs"/> ) : ( <Navigate to="/log-in"/> ) }/> 
+        <Route path="/log-in" element={<LoginForm/>}/>
+        <Route path="/sign-up" element={<SignupForm/>}/>
+        <Route path="/blogs" element={<BlogMenu/>}/>
+        <Route path="/blogs/:blogId" element={<BlogPage/>}/>
+      </Routes>
     </>
   )
 }
