91a25aa6a2bafedc9396992a7899dfa8eebe430a
lernEarnBurn <lerners124@gmail.com>
2023-10-31 13:20:27 +0200

    login handles incorrect login creds too

--- viewing-interface/src/components/LoginForm.tsx
+++ viewing-interface/src/components/LoginForm.tsx
@@ -1,24 +1,30 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import type { RefObject } from 'react';
 
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
 
 
 export function LoginForm(){
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const usernameRef: RefObject<HTMLInputElement> = useRef(null);
+  const passwordRef: RefObject<HTMLInputElement> = useRef(null);
+
+  const [showErrorMessage, setShowErrorMessage] = useState(false)
 
   async function checkLoginCreds(): Promise<void>{
-    
-
-    if(username !== '' && password !== ''){
+    if(usernameRef.current?.value && passwordRef.current?.value){
       axios.post('http://localhost:3000/log-in', {
-        username: username,
-        password: password
+        username: usernameRef.current?.value,
+        password: passwordRef.current?.value
       })
       .then(function (response) {
-        console.log(response);
+        if(response.data.token){
+          localStorage.setItem('token', response.data.token)
+          setShowErrorMessage(false)
+        }else{
+          setShowErrorMessage(true)
+        }
       })
       .catch(function (error) {
         console.log(error);
@@ -29,8 +35,9 @@ export function LoginForm(){
 
   return (
     <section>
-      <Input type="text" name="username" placeholder="Username" onChange={(e) => {setUsername(e.target.value)}}/>
-      <Input type="password" name="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
+      <Input ref={usernameRef} type="text" name="username" placeholder="Username"/>
+      <Input ref={passwordRef} type="password" name="password" placeholder="Password"/>
+      {showErrorMessage && <p className='error-message'>Login failed. Please check your credentials.</p>}
       <Button onClick={checkLoginCreds}>Submit</Button>
     </section>
   )
