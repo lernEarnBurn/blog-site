98c002fd292b622d3a64dfb2f8cbb873253b2b18
lernEarnBurn <lerners124@gmail.com>
2023-11-01 16:31:29 +0200

    built and implemented client side validation

--- viewing-interface/src/components/SignupForm.tsx
+++ viewing-interface/src/components/SignupForm.tsx
@@ -16,6 +16,10 @@ import {
 
 import { Link, useNavigate } from 'react-router-dom';
 
+import { signUpValidation } from '../../modules/validation';
+
+
+
 interface signUpFormProps {
   setLoggedIn:  React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -25,39 +29,55 @@ export function SignupForm(props: signUpFormProps) {
   const usernameRef: RefObject<HTMLInputElement> = useRef(null);
   const passwordRef: RefObject<HTMLInputElement> = useRef(null);
 
+  
   const [showErrorMessage, setShowErrorMessage] = useState(false)
+  const [errorMessage, setErrorMessage] = useState({usernameMessage: '', passwordMessage: ''})
 
   const navigate = useNavigate();
 
+  //add loading and kill buttons in this and in login
   async function submitNewUser(): Promise<void> {
-    try {
-      const response = await axios.post(
-        'http://localhost:3000/sign-up', {
-          username: usernameRef.current?.value.trim(),
-          password: passwordRef.current?.value.trim()
-      })
-  
-      //if it succeeds than auto login
-      if(response.data.user){
-        const logIn = await axios.post(
-          'http://localhost:3000/log-in', {
+    const error = signUpValidation(usernameRef.current?.value, passwordRef.current?.value)
+
+    if(error.passwordMessage === '' && error.usernameMessage === ''){
+      try {
+        setShowErrorMessage(false)
+
+        const response = await axios.post(
+          'http://localhost:3000/sign-up', {
             username: usernameRef.current?.value.trim(),
             password: passwordRef.current?.value.trim()
         })
-  
-        if(logIn.data.token){
-          localStorage.setItem('token', response.data.token)  
-          setShowErrorMessage(false)
-          props.setLoggedIn(true)
-          navigate('/')
+      
+        //if it succeeds than auto login
+        if(response.data.user){
+          const logIn = await axios.post(
+            'http://localhost:3000/log-in', {
+              username: usernameRef.current?.value.trim(),
+              password: passwordRef.current?.value.trim()
+          })
+        
+          if(logIn.data.token){
+            localStorage.setItem('token', response.data.token)  
+            setShowErrorMessage(false)
+            props.setLoggedIn(true)
+            navigate('/')
+          }
         }else{
-          setShowErrorMessage(true)
+          console.log('create user failed')
         }
-      }else{
-        console.log('create user failed')
+      } catch (err){
+        console.log(err)
       }
-    } catch (err){
-      console.log(err)
+    }else{
+      //ts work around if statement
+      if(error.usernameMessage !== undefined && error.passwordMessage !== undefined){
+        setErrorMessage({
+          usernameMessage: error.usernameMessage,
+          passwordMessage: error.passwordMessage,
+        });
+      }
+      setShowErrorMessage(true)
     }
     
   }
@@ -71,9 +91,11 @@ export function SignupForm(props: signUpFormProps) {
         </CardHeader>
         <CardContent>
           <Input ref={usernameRef} type="text" name="username" placeholder="Username"/>
+          {showErrorMessage && <p className='mt-1 ml-1 mb-[-.5vh] error-message text-xs'>{errorMessage.usernameMessage}</p>}
         </CardContent>
         <CardContent>
           <Input ref={passwordRef} type="password" name="password" placeholder="Password"/>
+          {showErrorMessage && <p className='mt-1 ml-1 mb-[-.5vh] error-message text-xs'>{errorMessage.passwordMessage}</p>}
         </CardContent>
         <CardContent>
           <Button className='w-full' onClick={submitNewUser}>Submit</Button>
