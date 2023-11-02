5d3c48ffd6ca84a73334fc95e7432fcda3c1ba6c
lernEarnBurn <lerners124@gmail.com>
2023-11-02 18:52:33 +0200

    blog menu structure

--- viewing-interface/src/components/BlogMenu.tsx
+++ viewing-interface/src/components/BlogMenu.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+interface Blog {
+  _id: string;
+  author: string;
+  content: string;
+  title: string;
+}
+
 export function BlogMenu() {
+
+  const [blogs, setBlogs] = useState<Blog[]>([])
+
+  useEffect(() => {
+    const getAllBlogs = async() =>{
+      try {
+        const response = await axios.get('http://localhost:3000/posts')
+        setBlogs(response.data)
+      } catch(err){
+        console.log(err)
+      }
+    }
+
+    getAllBlogs()
+  }, [])
+
+
   return (
-    <>this is the blog menu</>
+    <>
+      <div className="mt-[17vh] mb-[4vh] flex flex-col gap-4 overflow-y-auto h-auto min-w-[99vw] items-center">
+        {blogs.map((blog) => {
+          return <div className="rounded-lg dark:bg-opacity-90 mt-[3vh] py-2 px-3 border-2 light:border-black shadow-sm w-[40vw] h-[80vh] overflow-hidden" key={blog._id}>
+                    <h2 className="text-center text-2xl"><strong>{blog.title}</strong></h2>
+                    <p className="mt-2 text-md">{blog.content}</p>
+                 </div>
+        })}
+      </div>
+    </>
   )
 }
