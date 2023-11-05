aa89979be24a5f521ebb13e673711e295d99c4a3
lernEarnBurn <lerners124@gmail.com>
2023-11-05 20:15:58 +0200

    made and implement skeleton for loading page

--- viewing-interface/src/components/BlogMenu.tsx
+++ viewing-interface/src/components/BlogMenu.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom";
 
-import { Skeleton } from "./ui/skeleton";
+import { BlogSkeleton } from "./ui/BlogSkeleton";
 
 interface Blog {
   _id: string;
@@ -20,12 +20,15 @@ interface User {
 export function BlogMenu() {
 
   const [blogs, setBlogs] = useState<Blog[]>([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const getAllBlogs = async() =>{
+      setLoading(true)
       try {
         const response = await axios.get('http://localhost:3000/posts')
         setBlogs(response.data)
+        setLoading(false)
       } catch(err){
         console.log(err)
       }
@@ -55,19 +58,28 @@ export function BlogMenu() {
 
 
   return (
-    <>
-      <div className="mt-[17vh] mb-[4vh] flex flex-col gap-4 overflow-y-auto h-auto min-w-[99vw] items-center">
-        {blogs.map((blog, index) => {
-          return <div 
-                    ref={elem => itemsRef.current[index] = elem} 
-                    onClick={() => {transitionToBlogPage(blog, index)}} 
-                    className=" z-10 rounded-lg dark:bg-opacity-90 mt-[4.5vh] py-2 px-3 border-2 light:border-black shadow-sm w-[35vw] h-[87vh] overflow-hidden" 
-                    key={blog._id}>
-                    <h2 className="text-center text-2xl"><strong>{blog.title}</strong></h2>
-                    <h3 className="text-center text-md">By {blog.author && blog.author.username ? blog.author.username : 'changeWhenCanCreateBlogs'}</h3>
-                    <p className="mt-2 text-md">{blog.content}</p>
-                 </div>
-        })}
+     <>
+      <div className="mt-[13vh] mb-[4vh] flex flex-col gap-4 overflow-y-auto h-auto min-w-[99vw] items-center">
+        {!loading ? (
+          blogs.map((blog, index) => (
+            <div
+              ref={(elem) => (itemsRef.current[index] = elem)}
+              onClick={() => transitionToBlogPage(blog, index)}
+              className="z-10 rounded-lg dark:bg-opacity-90 mt-[4.5vh] py-2 px-10 border-2 light:border-black shadow-sm w-[35vw] h-[87vh] overflow-hidden"
+              key={blog._id}
+            >
+              <h2 className="text-center text-2xl">
+                <strong>{blog.title}</strong>
+              </h2>
+              <h3 className="text-center text-sm">
+                By {blog.author && blog.author.username ? blog.author.username : 'changeWhenCanCreateBlogs'}
+              </h3>
+              <p className="mt-2 text-md">{blog.content}</p>
+            </div>
+          ))
+        ) : (
+          <BlogSkeleton/>
+        )}
       </div>
     </>
   )
