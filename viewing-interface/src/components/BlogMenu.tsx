83d2c91cb9b86b547a9837c8f1e7bebdb5b53a5a
lernEarnBurn <lerners124@gmail.com>
2023-11-05 15:54:01 +0200

    clicking on a blog now scrolls that to center to prep for animation

--- viewing-interface/src/components/BlogMenu.tsx
+++ viewing-interface/src/components/BlogMenu.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import axios from "axios"
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface Blog {
   _id: string;
@@ -25,16 +25,39 @@ export function BlogMenu() {
 
     getAllBlogs()
   }, [])
+  
+  
+  const navigate = useNavigate()
+  const itemsRef = useRef<Array<HTMLDivElement | null>>([])
+
+  useEffect(() => {
+    itemsRef.current = itemsRef.current.slice(0, blogs.length);
+ }, [blogs]);
+
+  function transitionToBlogPage(blog: Blog, index: number): void{
+    if (itemsRef.current && itemsRef.current[index]) {
+      itemsRef.current[index]?.scrollIntoView({ behavior: "smooth",  block: "center" });
+    }
+   
+    
+    setTimeout(() => {
+      navigate(`/blogs/${blog._id}`);
+    }, 500);
+  }
 
 
   return (
     <>
       <div className="mt-[17vh] mb-[4vh] flex flex-col gap-4 overflow-y-auto h-auto min-w-[99vw] items-center">
-        {blogs.map((blog) => {
-          return <Link to={`/blogs/${blog._id}`} className="rounded-lg dark:bg-opacity-90 mt-[3vh] py-2 px-3 border-2 light:border-black shadow-sm w-[35vw] h-[80vh] overflow-hidden" key={blog._id}>
+        {blogs.map((blog, index) => {
+          return <div 
+                    ref={elem => itemsRef.current[index] = elem} 
+                    onClick={() => {transitionToBlogPage(blog, index)}} 
+                    className=" z-10 rounded-lg dark:bg-opacity-90 mt-[4.5vh] py-2 px-3 border-2 light:border-black shadow-sm w-[35vw] h-[87vh] overflow-hidden" 
+                    key={blog._id}>
                     <h2 className="text-center text-2xl"><strong>{blog.title}</strong></h2>
                     <p className="mt-2 text-md">{blog.content}</p>
-                 </Link>
+                 </div>
         })}
       </div>
     </>
