41873b386ea317a02b066d154231d536a300d290
lernEarnBurn <lerners124@gmail.com>
2023-11-07 20:48:44 +0200

    cleaned animation and did icons

--- viewing-interface/src/components/BackBar.tsx
+++ viewing-interface/src/components/BackBar.tsx
@@ -1,10 +1,12 @@
 import { motion } from 'framer-motion'
+import { ArrowLeftFromLine } from 'lucide-react';
+import { MessageCircle } from 'lucide-react';
+import { Share } from 'lucide-react';
 
 const animations = {
-  initial: {opacity: 0},
-  animate: {opacity: 1},
-  exit: {opacity: 0}
-
+  initial: {opacity: 0, x: -120},
+  animate: {opacity: 1, x: 0},
+  exit: {opacity: 0, x: -120},
 }
 
 interface BtnBarProps {
@@ -14,16 +16,16 @@ interface BtnBarProps {
 export const BtnBar = ({backFunc}: BtnBarProps) => {
   return (
     <motion.div
-      className='absolute top-[5vh] left-[25vw] flex flex-col gap-1' 
+      className='absolute top-[5vh] left-[25.5vw] flex flex-col gap-1' 
       variants={animations}
       initial="initial"
       animate="animate"
       exit="exit"
-      transition={{duration: 5.15, type: "tween", ease: "easeInOut"}}
+
       >
-      <button className='w-20 h-20 btn-bar hover:scale-[1.004]' onClick={backFunc}></button>
-      <button className='w-20 h-20 btn-bar hover:scale-[1.004]'></button>
-      <button className='w-20 h-20 btn-bar hover:scale-[1.004]'></button>
+      <button className='w-20 h-20 btn-bar hover:scale-[1.004] flex items-center justify-center' onClick={backFunc}><ArrowLeftFromLine className='icon w-20 h-16'/></button>
+      <button className='w-20 h-20 btn-bar hover:scale-[1.004] flex items-center justify-center'><MessageCircle className='icon w-20 h-16'/></button>
+      <button className='w-20 h-20 btn-bar hover:scale-[1.004] flex items-center justify-center'><Share className='icon w-20 h-16'/></button>
     </motion.div>
    
   )
